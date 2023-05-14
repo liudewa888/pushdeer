9fedfb5f6b0264b9c8d4ebb02b4680197d189704
liudewa888 <2549711475@qq.com>
2023-05-14 20:51:10 +0800

    新增: 程序异常退出通知

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
+import atexit
 import time
 from datetime import datetime
 
+from script.push import push
 from greenTea.greenTea import monitor_greenTea
 from bili.index import monitor_bili
-
+ii = 0
 def main():
-    ii = 0
+    global ii
     while(True):
       monitor_greenTea()
       monitor_bili()
@@ -14,6 +16,11 @@ def main():
       ii = ii + 1
       time.sleep(60 * 5)
 
+def cleanup():
+   global ii
+   if ii > 10:
+      push('监控异常退出: ','请登录检查')
+atexit.register(cleanup)
 if __name__ == '__main__':
     print('开始监控: ',datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
     main()
