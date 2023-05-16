e05c6228ccfbb6922db096949048647258e4e74c
liudewa888 <2549711475@qq.com>
2023-05-16 19:57:43 +0800

    新增: B站莫大置顶动态监控

--- bili/index.py
+++ bili/index.py
@@ -18,5 +18,47 @@ def monitor_bili():
     if(m_tg == ''):
         m_tg = id
     elif(m_tg != id):
-        push('B站莫大动态: ',type+' ' + text)
+        push('莫大最新动态: ',type+' ' + text)
         m_tg = id
+
+m_tg_top = ''
+noLogin = False
+def monitor_bili_moda():
+    global m_tg_top
+    global noLogin
+    if noLogin:
+        return
+    cookie = 'SESSDATA=6f01d815%2C1699699321%2C4b41b%2A51'
+    url = 'https://api.bilibili.com/x/polymer/web-dynamic/v1/feed/space?offset=&host_mid=525121722&timezone_offset=-480&features=itemOpusStyle'
+    headers={
+        'cookie':cookie
+    }
+    res = requests.get(url,headers=headers).json()
+
+    list = res['data']['items']
+    # print(list[0])
+    bool = False
+    jump_id = ''
+    for i in range(len(list)):
+        if(list[i]['modules']['module_tag']['text']=='置顶'):
+            basic = list[i]['basic']
+            readOnly = basic['is_only_fans']
+            if readOnly:
+              bool = True
+              jump_id = basic['jump_url'].split('/opus/')[1]
+            break
+    if bool:
+        url = f'https://api.bilibili.com/x/v2/reply/main?csrf=fcce6f152bd72daf7b7ca4e9db826f77&mode=3&oid={jump_id}&pagination_str=%7B%22offset%22:%22%22%7D&plat=1&seek_rpid=0&type=17'
+        res = requests.get(url,headers=headers).json()
+        reply = res['data']['top_replies'][0]
+        top_id = reply['rpid_str']
+        # print(reply['content']['message'])
+        if m_tg_top == '':
+            m_tg_top = top_id
+        elif top_id != m_tg_top:
+            top_msg = reply['content']['message'][0:16]
+            push('莫大置顶最新动态',top_msg)
+            m_tg_top = top_id
+    else:
+        push('莫大置顶动态失效','请重新登录')    
+        noLogin = True
--- main.py
+++ main.py
@@ -3,24 +3,28 @@ import time
 from datetime import datetime
 
 from script.push import push
-from greenTea.greenTea import monitor_greenTea
-from bili.index import monitor_bili
+from greenTea.index import monitor_greenTea
+from bili.index import monitor_bili,monitor_bili_moda
+from liqun.index import monitor_liqun
 ii = 0
 def main():
     global ii
     while(True):
-      monitor_greenTea()
-      monitor_bili()
       now = datetime.now()
       print("当前轮次" + str(ii) +': ', now.strftime("%Y-%m-%d %H:%M:%S"))
+      # monitor_greenTea()
+      # monitor_liqun()
+      monitor_bili()
+      monitor_bili_moda()
       ii = ii + 1
       time.sleep(60 * 5)
+      # time.sleep(10)
 
 def cleanup():
    global ii
    if ii > 10:
-      push('监控异常退出: ','请登录检查')
+      push('监控异常退出','请登录检查')
 atexit.register(cleanup)
 if __name__ == '__main__':
-    print('开始监控: ',datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+    print('------ 开始监控 ------')
     main()
